d7301d1158a5f1b66ece95cd5be0fcbdc37401bc
yamakai <restartsugar@gmail.com>
2020-01-04 17:27:00 +0900

    存在しないリポジトリ等の排除のためのerrorを作成．

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 import gcm_analyzer.repo_metrics.cloc_analyzer.cloc_analyzer as ca
 import gcm_analyzer.repo_metrics.github_api_analyzer.issue_analyzer as ia
 import gcm_analyzer.repo_metrics.github_api_analyzer.label_analyzer as la
+from gcm_analyzer.gcm_error.gcm_error import RepositoryList
 import json
 import pandas as pd
 
 
 def main():
     with open('test_out.json', 'r') as f:
-        result = json.load(f)
-    ca.get_all_repo_cloc(lang_list=['Python', 'Markdown'],
-                         loc_args=['comment', 'blank', 'code'],
-                         repo_list=['LakeRainSound/get_code_metrics',
-                                    'LakeRainSound/sample-java-project'],
-                         repo_result=result['repository'])
-    issue_result = ia.get_all_issues_info(repo_list=['LakeRainSound/get_code_metrics',
-                                                     'LakeRainSound/sample-java-project'],
-                                          repo_result=result['repository'])
+        gcm_result = json.load(f)
 
-    label_result = la.get_all_label_info(repo_list=['LakeRainSound/get_code_metrics',
-                                                    'LakeRainSound/sample-java-project'],
-                                         repo_result=result['repository'])
-    issue_result = pd.DataFrame(issue_result)
-    label_result = pd.DataFrame(label_result)
-    print(pd.merge(issue_result, label_result, on='nameWithOwner'))
+    gcme = RepositoryList(repository_list=['LakeRainSound/get_code_metrics',
+                                           'LakeRainSound/sample-java-project',
+                                           'LakeRainSound/sample-java-project',
+                                           'LakeRainSound/empty',
+                                           'lslakaflkjc/vhjksfhkavvn',
+                                           'sssffaf/sjkhfkjjf'],
+                          gcm_result=gcm_result['repository'],
+                          number_of_repo=4)
+    no_error_repo_list = gcme.get_no_error_repo_list()
+    print(no_error_repo_list)
     return
 
 
