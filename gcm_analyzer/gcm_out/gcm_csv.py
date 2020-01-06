1ce874d73a395d004b4ed0b934a4e665650eafe2
yamakai <restartsugar@gmail.com>
2020-01-06 20:14:03 +0900

    file構成を変更．issue pageがないrepositoryを計上しない出力の追加．

--- gcm_analyzer/gcm_out/gcm_csv.py
+++
@@ -1,8 +0,0 @@
-from pandas import DataFrame
-from pathlib import Path
-
-
-def out_csv(result: DataFrame, path_to_output_file: Path):
-    result.to_csv(str(path_to_output_file),
-                  index=False,
-                  encoding='utf-8')
--- gcm_analyzer/gcm_out/gcm_out.py
+++ gcm_analyzer/gcm_out/gcm_out.py
@@ -1,8 +1,26 @@
 from pathlib import Path
-from gcm_analyzer.gcm_out.gcm_csv import out_csv
-from gcm_analyzer.gcm_out.gcm_quartile import out_quartile
 
 from pandas import DataFrame
+
+
+# 値の四分位数を取得
+def out_quartile(result: DataFrame):
+    print(result.quantile([0, 0.25, 0.5, 0.75, 1.0]))
+
+
+# csvに出力
+def out_csv(result: DataFrame, path_to_output_file: Path):
+    result.to_csv(str(path_to_output_file),
+                  index=False,
+                  encoding='utf-8')
+
+
+def out_issues_enabled(result: DataFrame):
+    has_issue_unenabled_list = result[result['hasIssuesEnabled'] != True]
+    print('\nIssue Page Enabled Repository per All ')
+    print('{}% ({}/{})'.format(len(has_issue_unenabled_list)/len(result)*100,
+                               len(has_issue_unenabled_list),
+                               len(result)))
 
 
 def _get_dataframe_merged_dict(result_list):
@@ -16,9 +34,16 @@ def _get_dataframe_merged_dict(result_list):
 
 def gcm_out_call(result_list, path_to_output_file: Path):
     result = _get_dataframe_merged_dict(result_list)
-
+    # 全てのリポジトリ
+    print('\n**** ALL Repositories ****\n')
     # csvに出力
     out_csv(result, path_to_output_file)
     # 四分位数を取得
     out_quartile(result)
 
+    # issueページが利用可能かどうかを示す．
+    out_issues_enabled(result)
+
+    print('\n**** Repositories which have Issue Page ****\n')
+    # 四分位数を取得(Issue Pageが利用可能な場合)
+    out_quartile(result[result['hasIssuesEnabled']])
